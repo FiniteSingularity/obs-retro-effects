731961d4ffeb91bb48ec46a85b5714f7b7367f58
FiniteSingularity <finitesingularityttv@gmail.com>
2024-03-15 20:02:57 -0500

    Removes web app.

--- matrix-rain-webapp/src/app/app.config.ts
+++
@@ -1,9 +0,0 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
-import { NbThemeModule } from '@nebular/theme';
-
-export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), importProvidersFrom(NbThemeModule.forRoot({name: 'dark'}))]
-};
