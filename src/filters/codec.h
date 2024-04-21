d64bbe328ed9aa892cf11ad55a3abb27fa695139
Millennium Cyborg <79787803+y2kcyborg@users.noreply.github.com>
2024-04-21 15:45:41 +0100

    Add new FMV filter (#20)
    * Add FMV filter
    * Mark unused params to fix mac build
    * FMV: Don't process in srgb space, it actually looks worse and also handles a pure white input badly
    * FMV: Rescale error values and avoid a few sqrts()
    * FMV: Better balanced quality slider
    * FMV: Rename from 'FMV' to 'Retro Codec' and add drop-down for future alternative codecs

---
+++ src/filters/codec.h
@@ -0,0 +1,55 @@
+#include <obs-module.h>
+#include "../obs-retro-effects.h"
+
+#define CODEC_TYPE_RPZA 1
+#define CODEC_TYPE_RPZA_LABEL "RetroEffects.Codec.RPZA"
+
+struct codec_filter_data;
+typedef struct codec_filter_data codec_filter_data_t;
+
+struct codec_filter_data {
+	gs_effect_t *effect_fmv;
+
+	gs_eparam_t *param_image;
+	gs_eparam_t *param_uv_size;
+	gs_eparam_t *param_prev_frame;
+	gs_eparam_t *param_prev_frame_valid;
+
+	gs_eparam_t *param_colors_per_channel;
+	gs_eparam_t *param_threshold_prev_frame;
+	gs_eparam_t *param_threshold_solid;
+	gs_eparam_t *param_threshold_gradient;
+
+	gs_texrender_t *texrender_downsampled_input;
+	gs_texrender_t *texrender_previous_frame;
+	gs_texrender_t *texrender_downsampled_output;
+
+	uint32_t codec_type;
+
+	float px_scale;
+	int colors_per_channel;
+
+	float quality;
+	bool custom_thresholds;
+	
+	float threshold_prev_frame;
+	float threshold_solid;
+	float threshold_gradient;
+
+	bool loading_effect;
+	bool reload_effect;
+};
+
+extern void codec_create(retro_effects_filter_data_t *filter);
+extern void codec_destroy(retro_effects_filter_data_t *filter);
+extern void codec_filter_video_render(retro_effects_filter_data_t *data);
+extern void codec_filter_properties(retro_effects_filter_data_t *data,
+					obs_properties_t *props);
+extern void codec_filter_defaults(obs_data_t *settings);
+extern void codec_filter_update(retro_effects_filter_data_t *data,
+				    obs_data_t *settings);
+static void codec_set_functions(retro_effects_filter_data_t *filter);
+static void codec_load_effect(codec_filter_data_t *filter);
+
+static bool codec_type_modified(obs_properties_t *props, obs_property_t *p,
+				 obs_data_t *settings);
